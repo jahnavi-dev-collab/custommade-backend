bd1b6c5de7ed8271f9c364ef8e410048fd1c7ff1
jahnavi-dev-collab <jahnaviketha16@gmail.com>
2026-01-13 22:50:35 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/custommade/platform/CustommadeApplication.java
@@ -0,0 +1,13 @@
+package com.custommade.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustommadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustommadeApplication.class, args);
+    }
+
+}
